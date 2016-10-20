1bec74017733ff831f9d88daac1f133aa604ba96
Dmitry Misiulia <d.misiulia@hackspace.by>
2016-10-20 10:44:03 +0300

    Added Action mode like dzone.com

--- app/src/main/java/com/example/toolbar/SecondActivity.java
+++ app/src/main/java/com/example/toolbar/SecondActivity.java
@@ -1,14 +1,62 @@
 package com.example.toolbar;
 
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class SecondActivity extends AppCompatActivity {
+
+    private ActionMode mActionMode;
+    private ActionBar mActionBar;
+    private String[] mas = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    private SelectionAdapter mAdapter;
+
+//    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+//        @Override
+//        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+//            MenuInflater inflater = mode.getMenuInflater();
+//            inflater.inflate(R.menu.action_mode_menu, menu);
+//            return true;
+//        }
+//
+//        @Override
+//        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+//            return false;
+//        }
+//
+//        @Override
+//        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+//
+//            switch (item.getItemId()) {
+//                case R.id.item_delete:
+//                    mode.finish();
+//                    return true;
+//                default:
+//                    return false;
+//            }
+//        }
+//
+//        @Override
+//        public void onDestroyActionMode(ActionMode mode) {
+//            mActionMode = null;
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +64,64 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
-        ActionBar bar = getSupportActionBar();
-        bar.setDisplayShowTitleEnabled(false);
-        bar.setDisplayHomeAsUpEnabled(true);
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        createListView();
+
+    }
+
+    private void createListView() {
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        mAdapter = new SelectionAdapter(this, android.R.layout.simple_list_item_1, mas);
+        listView.setAdapter(mAdapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+            private int nr = 0;
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+                if (b){
+                    nr++;
+                    mAdapter.setNewSelection(i, b);
+                } else {
+                    nr--;
+                    mAdapter.removeSelection(i);
+                }
+                actionMode.setTitle(nr + " selected");
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                nr = 0;
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.action_mode_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.item_delete:
+                        nr = 0;
+                        mAdapter.clearSelection();
+                        actionMode.finish();
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+                mAdapter.clearSelection();
+            }
+        });
+
     }
 
     @Override
@@ -42,4 +145,49 @@ public class SecondActivity extends AppCompatActivity {
         }
     }
 
+    private class SelectionAdapter extends ArrayAdapter<String> {
+
+        private HashMap<Integer, Boolean> mSelection = new HashMap<Integer, Boolean>();
+
+        public SelectionAdapter(Context context, int resource,
+                                String[] objects) {
+            super(context, resource, objects);
+        }
+
+        public void setNewSelection(int position, boolean value) {
+            mSelection.put(position, value);
+            notifyDataSetChanged();
+        }
+
+        public boolean isPositionChecked(int position) {
+            Boolean result = mSelection.get(position);
+            return result == null ? false : result;
+        }
+
+        public Set<Integer> getCurrentCheckedPosition() {
+            return mSelection.keySet();
+        }
+
+        public void removeSelection(int position) {
+            mSelection.remove(position);
+            notifyDataSetChanged();
+        }
+
+        public void clearSelection() {
+            mSelection = new HashMap<Integer, Boolean>();
+            notifyDataSetChanged();
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = super.getView(position, convertView, parent);//let the adapter handle setting up the row views
+            v.setBackgroundColor(getResources().getColor(android.R.color.background_light)); //default color
+
+            if (mSelection.get(position) != null) {
+                v.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));// this is a selected position so make it red
+            }
+            return v;
+        }
+    }
 }
